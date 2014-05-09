d6f73fac6832e904f9bbc3389f11e1e55b95f73f
Michael Klopf <klopf.michael@gmail.com>
2014-05-09 13:37:13 +0200

    Config of app.

--- app/app.js
+++ app/app.js
@@ -1,1 +1,9 @@
-angular.module('Guidebook', []);
+// Application starting point
+var appConfig = function($routeProvider) {
+  $routeProvider.when("/", {
+    controller : "ChaptersController", // dies zeigt nicht auf das js File
+    templateURL : "view/chapter.html"
+  });
+};
+
+var App = angular.module('Guidebook', []).config(appConfig);
